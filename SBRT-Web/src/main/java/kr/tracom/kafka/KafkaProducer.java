12335b9e30a0c6fd445c9157961d01f5686940a3
bhmin <bhmin@tracom.kr>
2021-04-30 13:03:39 +0900

    kafka 적용
    - 필요 라이브러리 dependency 및 파일 추가
      ; kafka, sleuth, zipkin
      ; Tims 라이브러리
    - kafka consumer, producer 클래스 추가

---
+++ SBRT-Web/src/main/java/kr/tracom/kafka/KafkaProducer.java
@@ -0,0 +1,26 @@
+package kr.tracom.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import kr.tracom.platform.service.kafka.model.KafkaMessage;
+
+@Component
+public class KafkaProducer {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private KafkaTemplate<Object, Object> kafkaTemplate;
+
+    @Async
+    public void produceTimsMessage(String topic, KafkaMessage message) {
+    	
+    	//logger.info("produceTimsMessage()");
+    	
+        kafkaTemplate.send(topic, message);
+    }
+}
