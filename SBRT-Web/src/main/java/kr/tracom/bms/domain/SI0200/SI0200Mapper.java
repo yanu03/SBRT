21cce83c71fb12079de09c0bfcdc54934453c321
hwyang <hwyang@tracom.kr>
2021-06-04 09:16:29 +0900

    [양현우] 차량관리 진행중

---
+++ SBRT-Web/src/main/java/kr/tracom/bms/domain/SI0200/SI0200Mapper.java
@@ -0,0 +1,22 @@
+package kr.tracom.bms.domain.SI0200;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SI0200Mapper {
+
+	public List SI0200G0R0(Map param);
+	
+	public List SI0200P0R0();
+	
+	public Map SI0200G0K0();
+	
+	public int SI0200G0I0(Map param);
+	
+	public int SI0200G0D0(Map param);
+	
+	public int SI0200G0U0(Map param);
+}
