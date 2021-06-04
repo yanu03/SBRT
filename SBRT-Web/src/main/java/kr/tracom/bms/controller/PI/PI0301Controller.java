41c9666a9876f2e137e781f4ab28b067408e9821
khw <hwkang@tracom.kr>
2021-06-04 09:26:44 +0900

    [강현우] 뉴스설정, 뉴스관리 레이아웃 추가

---
+++ SBRT-Web/src/main/java/kr/tracom/bms/controller/PI/PI0301Controller.java
@@ -0,0 +1,39 @@
+package kr.tracom.bms.controller.PI;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.bms.domain.PI0301.PI0301Service;
+import kr.tracom.cm.support.ControllerSupport;
+
+
+@Controller
+@Scope("request")
+public class PI0301Controller extends ControllerSupport {
+
+	@Autowired
+	private PI0301Service PI0301Service;
+
+	@RequestMapping("/pi/PI0301G0R0")
+	public @ResponseBody Map<String, Object> PI0301G0R0() throws Exception {
+		result.setData("dlt_BMS_NEWS_CFG_INFO", PI0301Service.PI0301G0R0());
+		return result.getResult();
+	}	
+	
+	@RequestMapping("/pi/PI0301G0S0")
+	public @ResponseBody Map<String, Object> PI0301G0S0() throws Exception{
+		result.setData("result", PI0301Service.PI0301G0S0());
+		return result.getResultSave();
+	}
+	
+	@RequestMapping("/pi/PI0301G0K0")
+	public @ResponseBody Map<String, Object> PI0301G0K0() throws Exception{
+		result.setData("dma_SEQ_BMS_NEWS_CFG_0", PI0301Service.PI0301G0K0());
+		return result.getResult();
+	}
+}
