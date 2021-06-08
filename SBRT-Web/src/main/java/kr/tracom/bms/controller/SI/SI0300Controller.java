59179fc7ca8252ac606efff6ad80d95bfa9054e7
hwyang <hwyang@tracom.kr>
2021-06-08 15:12:53 +0900

    [양현우] 운전자 관리 페이지, 이미지 업/다운로드 미구현

---
+++ SBRT-Web/src/main/java/kr/tracom/bms/controller/SI/SI0300Controller.java
@@ -0,0 +1,52 @@
+package kr.tracom.bms.controller.SI;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.bms.domain.SI0300.SI0300Service;
+import kr.tracom.cm.support.ControllerSupport;
+
+@Controller
+@Scope("request")
+public class SI0300Controller extends ControllerSupport{
+	
+	@Autowired
+	private SI0300Service si0300Service;
+	
+	@RequestMapping("/si/SI0300G0R0")
+	public @ResponseBody Map<String, Object> SI0300G0R0() throws Exception {
+		result.setData("dlt_BMS_DRV_MST", si0300Service.SI0300G0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/si/SI0300P0R0")
+	public @ResponseBody Map<String, Object> SI0300P0R0() throws Exception {
+		result.setData("dlt_TRANSCOMP_MST", si0300Service.SI0300P0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/si/SI0300SHI0")
+	public @ResponseBody Map<String, Object> SI0102G0R2() throws Exception {
+		result.setData("dlt_searchitem", si0300Service.SI0300SHI0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/si/SI0300G0K0")
+	public @ResponseBody Map<String, Object> SI0300G0K0() throws Exception {
+		result.setData("dma_SEQ_BMS_DRV_MST_0", si0300Service.SI0300G0K0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/si/SI0300G0S0")
+	public @ResponseBody Map<String, Object> SI0300G0S0() throws Exception {
+		Map map = si0300Service.SI0300G0S0();
+		result.setData("dma_result", map);
+		return result.getResultSave();
+	}	
+	
+}
---
+++ SBRT-Web/src/main/java/kr/tracom/bms/domain/SI0300/SI0300Mapper.java
@@ -0,0 +1,24 @@
+package kr.tracom.bms.domain.SI0300;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SI0300Mapper {
+	
+	public List SI0300G0R0(Map param);
+	
+	public List SI0300P0R0();
+	
+	public List SI0300SHI0();
+	
+	public Map SI0300G0K0();
+	
+	public int SI0300G0I0(Map param);
+	
+	public int SI0300G0D0(Map param);
+	
+	public int SI0300G0U0(Map param);
+}
