6e70957ebf864d1bb166640f03fc78b1a9c04de9
bhmin <bhmin@tracom.kr>
2021-07-30 17:09:37 +0900

    [민병희] TIMS 라이브러리 업데이트
    - TIMS 라이브러리 업데이트(2021.07.28 데이터정의서 기준)
    - 운행이벤트 수신 시 이력 insert 추가

--- SBRT-Web/src/main/java/kr/tracom/tims/handler/GetResponse.java
+++ SBRT-Web/src/main/java/kr/tracom/tims/handler/GetResponse.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import kr.tracom.platform.attribute.BrtAtCode;
-import kr.tracom.platform.attribute.brt.AtBusInfo;
 import kr.tracom.platform.net.protocol.TimsMessage;
 import kr.tracom.platform.net.protocol.attribute.AtMessage;
 import kr.tracom.platform.net.protocol.payload.PlGetResponse;
@@ -26,26 +24,7 @@ public class GetResponse {
         for(AtMessage atMessage : payload.getAttrList()){
             short attrId = atMessage.getAttrId();
 
-            switch(attrId){
-
-                case BrtAtCode.DEVICE_LOCATION_INFO:
-                    break;
-                case BrtAtCode.DEVICE_STATUS_INFO:
-                    break;
-                case BrtAtCode.BUS_INFO:
-                	AtBusInfo busInfo = (AtBusInfo)atMessage.getAttrData();
-
-                	//test [[
-                	/*
-                	logger.info("lati:{}, longi:{}", busInfo.getLati(), busInfo.getLongi());
-                	
-                	wsMsg = new WsMessage();
-                	wsMsg.setContent("VH00000001");
-                	*/
-                	//test ]]
-                	
-
-                    break;
+            switch(attrId){                
                 default:
                     break;
 
