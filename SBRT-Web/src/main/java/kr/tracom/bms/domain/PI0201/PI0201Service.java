53a23d0622e3f907a470b3c0271ce0cb87da964a
bsko <bsko@tracom.kr>
2021-08-03 14:27:16 +0900

    [고병수] 정류장안내(음성, LED) 관리  재생시간, 미리듣기

--- SBRT-Web/src/main/java/kr/tracom/bms/domain/PI0201/PI0201Service.java
+++ SBRT-Web/src/main/java/kr/tracom/bms/domain/PI0201/PI0201Service.java
@@ -5,10 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import kr.tracom.cm.support.ServiceSupport;
+import kr.tracom.cm.support.exception.MessageException;
 import kr.tracom.util.DateUtil;
+import kr.tracom.util.Result;
 
 @Service
 public class PI0201Service extends ServiceSupport{
@@ -17,10 +20,8 @@ public class PI0201Service extends ServiceSupport{
 	private PI0201Mapper PI0201Mapper;
 		
 	public List<Map> PI0201G0R0() throws Exception{
-		Map param = getSimpleDataMap("dma_search");
-		return PI0201Mapper.PI0201G0R0(param);
-		
-		/*List returnList = PI0201Mapper.PI0201G0R0(param);
+		Map<String, Object> param = getSimpleDataMap("dma_search");
+		List returnList = PI0201Mapper.PI0201G0R0(param);
 		
 		Map<String, Object> AUDIO_INFO = getSimpleDataMap("dma_AUDIO_INFO");
 		for(Object obj:returnList) {
@@ -29,7 +30,7 @@ public class PI0201Service extends ServiceSupport{
 			temp.put("VOC_PATH", "/fileUpload/audio/"+AUDIO_INFO.get("AUDIO_NM"));			
 		}
 		
-		return returnList;		*/
+		return returnList;
 		
 	}
 	
@@ -45,34 +46,51 @@ public class PI0201Service extends ServiceSupport{
 		int iCnt = 0;
 		int uCnt = 0;
 		int dCnt = 0;
-		List param = getSimpleList("dlt_BMS_VOC_INFO");
+		
+		List<Map<String, Object>> param = getSimpleList("dlt_BMS_VOC_INFO");
         Map<String, Object> AUDIO_INFO = getSimpleDataMap("dma_AUDIO_INFO");
-		for (int i = 0; i < param.size(); i++) {
-			Map<String, Object> data = (Map) param.get(i);
-			String rowStatus = (String) data.get("rowStatus");
-			// 데이터베이스 date 타입일때 공백으로 들어가면 에러나는 사항 임시 수정
-			for (String key : data.keySet()) {
-				if (data.get(key).equals("")) {
-					data.put(key, null);
-				}
-			}			
-			if (rowStatus.equals("C")) {
-				iCnt += PI0201Mapper.PI0201G0I0(data);
-			} else if (rowStatus.equals("U")) {
-				uCnt += PI0201Mapper.PI0201G0U0(data);
-				
-                doMoveFile("up/", "audio/", AUDIO_INFO.get("AUDIO_NM").toString(), AUDIO_INFO.get("AUDIO_NM").toString());
-                
-				
-			} else if (rowStatus.equals("D")) {
-				dCnt += PI0201Mapper.PI0201G0D0(data);
+        
+        try {
+        	for (int i = 0; i < param.size(); i++) {
+    			Map<String, Object> data = param.get(i);
+    			String rowStatus = (String) data.get("rowStatus");
+    			// 데이터베이스 date 타입일때 공백으로 들어가면 에러나는 사항 임시 수정
+    			for (String key : data.keySet()) {
+    				if (data.get(key).equals("")) {
+    					data.put(key, null);
+    				}
+    			}			
+    			if (rowStatus.equals("C")) {
+    				iCnt += PI0201Mapper.PI0201G0I0(data);
+    				
+    				if((AUDIO_INFO.get("AUDIO_NM")!=null)&&(AUDIO_INFO.get("AUDIO_NM").toString().isEmpty()==false))
+						{
+    						doMoveFile("up/", "audio/", AUDIO_INFO.get("AUDIO_NM").toString(), AUDIO_INFO.get("AUDIO_NM").toString());
+						}
+    				
+    			} else if (rowStatus.equals("U")) {
+    				uCnt += PI0201Mapper.PI0201G0U0(data);
+   
+    				if((AUDIO_INFO.get("AUDIO_NM")!=null)&&(AUDIO_INFO.get("AUDIO_NM").toString().isEmpty()==false))
+						{
+    						doMoveFile("up/", "audio/", AUDIO_INFO.get("AUDIO_NM").toString(), AUDIO_INFO.get("AUDIO_NM").toString());
+						}
+                    
+    				
+    			} else if (rowStatus.equals("D")) {
+    				dCnt += PI0201Mapper.PI0201G0D0(data);
+    			}
+    		}
+		} catch (Exception e) {
+			if (e instanceof DuplicateKeyException) {
+				throw new MessageException(Result.ERR_KEY, "중복된 키값의 데이터가 존재합니다.");
+			} else {
+				throw e;
 			}
 		}
-		Map result = new HashMap();
-		result.put("STATUS", "S");
-		result.put("ICNT", String.valueOf(iCnt));
-		result.put("UCNT", String.valueOf(uCnt));
-		result.put("DCNT", String.valueOf(dCnt));
+		
+		Map result = saveResult(iCnt, uCnt, dCnt);
+		
 		return result;
 	}
 }
