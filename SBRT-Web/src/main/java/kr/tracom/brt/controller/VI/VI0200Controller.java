3cb9a4631e167422e0f81100e6e972e5f7cec006
bsko <bsko@tracom.kr>
2021-08-17 09:34:08 +0900

    [고병수] 민원 관리 페이지

---
+++ SBRT-Web/src/main/java/kr/tracom/brt/controller/VI/VI0200Controller.java
@@ -0,0 +1,61 @@
+package kr.tracom.brt.controller.VI;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.bms.domain.SI0101.SI0101Service;
+import kr.tracom.brt.domain.MO0302.MO0302Service;
+import kr.tracom.brt.domain.VI0200.VI0200Service;
+import kr.tracom.cm.support.ControllerSupport;
+
+@Controller
+@Scope("request")
+public class VI0200Controller extends ControllerSupport {
+	
+	@Autowired
+	private VI0200Service vi0200Service;
+	
+	@RequestMapping("/vi/VI0200G0R0")
+	public @ResponseBody Map<String, Object> VI0200G0R0() throws Exception {
+		result.setData("dlt_BRT_CPLNT_HIS", vi0200Service.VI0200G0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/vi/VI0200SHI0")
+	public @ResponseBody Map<String, Object> VI0200SHI0() throws Exception {
+		result.setData("dlt_grgSearchItem", vi0200Service.VI0200SHI0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/vi/VI0200G0S0")
+	public @ResponseBody Map<String, Object> VI0200G0S0() throws Exception{
+		Map map = vi0200Service.VI0200G0S0();
+		result.setData("dma_result", map);
+		return result.getResultSave();
+	}
+	
+	@RequestMapping("/vi/VI0200G0K0")
+	public @ResponseBody Map<String, Object> VI0200G0K0() throws Exception{
+		result.setData("dma_SEQ_BRT_CPLNT_HIS_0", vi0200Service.VI0200G0K0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/vi/VI0200P0R0")
+	public @ResponseBody Map<String, Object> VI0200P0R0() throws Exception {
+		result.setData("dlt_BRT_CPLNT_HIS", vi0200Service.VI0200P0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping(value = "/vi/VI0200P0S0")
+	public @ResponseBody Map<String, Object> VI0200P0S0() throws Exception {
+		result.setData("dma_result", vi0200Service.VI0200P0S0());
+
+		return result.getResultSave();
+	}
+	
+}
---
+++ SBRT-Web/src/main/java/kr/tracom/brt/domain/VI0200/VI0200Mapper.java
@@ -0,0 +1,33 @@
+package kr.tracom.brt.domain.VI0200;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface VI0200Mapper {
+
+	public List VI0200G0R0(Map param);
+	
+	public List VI0200SHI0();
+	
+	public Map VI0200G0K0();
+	
+	public int VI0200G0U0(Map param);
+	
+	public int VI0200G0I0(Map param);
+	
+	public int VI0200G0D0(Map param);
+	
+	public List VI0200P0R0(Map param);
+	
+	public int VI0200P0I0(Map param);
+	
+	public int VI0200P0D0(Map param);
+	
+	public int VI0200P0U0(Map param);
+	
+	public int VI0200P0DA0(Map param);
+	
+}
