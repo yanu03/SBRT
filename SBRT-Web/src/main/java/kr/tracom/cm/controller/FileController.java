27d7fd4f3918d4046954332da943d1d84867bc51
bsj9010 <sjbaek@tracom.kr>
2021-09-03 00:20:52 +0900

    [백승종] 수정 사항 적용

--- SBRT-Web/src/main/java/kr/tracom/cm/controller/FileController.java
+++ SBRT-Web/src/main/java/kr/tracom/cm/controller/FileController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
