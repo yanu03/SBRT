be748dcd7980bee99b1b1d102108c66d0e2a4d3a
bsko <bsko@tracom.kr>
2021-09-07 17:32:45 +0900

    [고병수] 위반 이력 관리

--- SBRT-Web/src/main/java/kr/tracom/brt/domain/VI0100/VI0100Service.java
+++ SBRT-Web/src/main/java/kr/tracom/brt/domain/VI0100/VI0100Service.java
@@ -22,6 +22,11 @@ public class VI0100Service extends ServiceSupport {
 		return vi0100Mapper.VI0100G0R0(map);
 	}
 	
+	public List VI0100P0R0() throws Exception {
+		Map<String, Object> map = getSimpleDataMap("dma_search");
+		return vi0100Mapper.VI0100P0R0(map);
+	}
+	
 	public List VI0100SHI0() throws Exception {
 		return vi0100Mapper.VI0100SHI0();
 		
@@ -33,12 +38,22 @@ public class VI0100Service extends ServiceSupport {
 		int dCnt = 0;		
 		
 		List<Map<String, Object>> param = getSimpleList("dlt_BRT_OPER_VIOLT_HIS");
+		Map map = getSimpleDataMap("dma_param_VI0100G1");
 		try {
 			for (int i = 0; i < param.size(); i++) {
 				Map data = (Map) param.get(i);
 				String rowStatus = (String) data.get("rowStatus");
 				if (rowStatus.equals("C")) {
+					
+					data.put("OPER_DT",map.get("OPER_DT"));
+					data.put("REP_ROUT_ID",map.get("REP_ROUT_ID"));
+					data.put("ROUT_ID",map.get("ROUT_ID"));
+					data.put("VHC_ID",map.get("VHC_ID"));
+					data.put("VHC_NO",map.get("VHC_NO"));
+					
 					iCnt += vi0100Mapper.VI0100G1I0(data);
+				} else if (rowStatus.equals("U")) {
+					uCnt += vi0100Mapper.VI0100G1U0(data);
 				} else if (rowStatus.equals("D")) {
 					dCnt += vi0100Mapper.VI0100G1D0(data);
 				} 
