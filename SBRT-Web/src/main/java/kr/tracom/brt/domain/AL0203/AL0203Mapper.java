68fa14f78aa68e4e23a930436a6e48833cd92cd8
bsko <bsko@tracom.kr>
2021-09-15 17:36:28 +0900

    [고병수] 운행계획 관리 임시

---
+++ SBRT-Web/src/main/java/kr/tracom/brt/domain/AL0203/AL0203Mapper.java
@@ -0,0 +1,34 @@
+package kr.tracom.brt.domain.AL0203;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AL0203Mapper {
+	
+	public List AL0203G0R0(Map param);
+	
+	public List AL0203SHI0();
+	
+	public List AL0203G1R0(Map param);
+	/*
+	public List AL0103P0R0(Map param);
+	
+	public int AL0103G0I0(Map param);
+	
+	public int AL0103G0D0(Map param);
+	
+	public int AL0103G0U0(Map param);
+	
+	public int AL0103G1I0(Map param);
+	
+	public int AL0103G1D0(Map param);
+	
+	public int AL0103G1U0(Map param);
+	
+	public List AL0103P01R0(Map param);
+	
+	public List AL0103P1SH(Map param);*/
+}
