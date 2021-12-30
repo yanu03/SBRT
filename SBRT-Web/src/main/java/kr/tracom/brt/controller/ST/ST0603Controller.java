fc95bb06df54c9c4be369c4617b4a83a3a4f240e
DESKTOP-7OD55LH\yongdeok <ydkim033@naver.com>
2021-12-30 16:31:30 +0900

    [김용덕] 변경운행 날짜별 통계 페이지

---
+++ SBRT-Web/src/main/java/kr/tracom/brt/controller/ST/ST0603Controller.java
@@ -0,0 +1,45 @@
+package kr.tracom.brt.controller.ST;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.tracom.brt.domain.ST0603.ST0603Service;
+import kr.tracom.cm.support.ControllerSupport;
+
+@Controller
+@Scope("request")
+public class ST0603Controller extends ControllerSupport {
+
+	@Autowired
+	private ST0603Service st0603Service;
+	
+	@RequestMapping("/st/ST0603G0R0")
+	public @ResponseBody Map<String, Object> ST0603G0R0() throws Exception {
+		result.setData("dlt_BMS_ROUT_MST", st0603Service.ST0603G0R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/st/ST0603G1R0")
+	public @ResponseBody Map<String, Object> ST0603G1R0() throws Exception {
+		result.setData("dlt_BRT_OPER_VIOLT_STAT", st0603Service.ST0603G1R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/st/ST0603G2R0")
+	public @ResponseBody Map<String, Object> ST0603G2R0() throws Exception {
+		result.setData("dlt_BRT_OPER_VIOLT_STAT2", st0603Service.ST0603G2R0());
+		return result.getResult();
+	}
+	
+	@RequestMapping("/st/ST0603SHI0")
+	public @ResponseBody Map<String, Object> ST0603SHI0() throws Exception {
+		result.setData("dlt_searchitem", st0603Service.ST0603SHI0());
+		return result.getResult();
+	}
+	
+}
---
+++ SBRT-Web/src/main/java/kr/tracom/brt/domain/ST0603/ST0603Mapper.java
@@ -0,0 +1,19 @@
+package kr.tracom.brt.domain.ST0603;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ST0603Mapper {
+
+	public List ST0603G0R0(Map param);
+	
+	public List ST0603G1R0(Map param);
+	
+	public List ST0603G2R0(Map param);
+	
+	public List ST0603SHI0(Map param);
+
+}
