77a89fadf513ebce685fb836288f57dee01a589b
bsko <bsko@tracom.kr>
2022-01-03 15:24:08 +0900

    [고병수] 운행 정시성 통계, 정류소 정시성 통계. 통계 날짜 수정.

---
+++ SBRT-Web/src/main/java/kr/tracom/brt/domain/ST0803/ST0803Mapper.java
@@ -0,0 +1,16 @@
+package kr.tracom.brt.domain.ST0803;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ST0803Mapper {
+
+	public List ST0803G0R0(Map param);
+	
+	public List ST0803G1R0(Map param);
+	
+	public List ST0803G2R0(Map param);
+}
