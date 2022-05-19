0100dd4ab69167584727cbbf3294d776898ebcbc
bsko <bsko@tracom.kr>
2022-05-19 16:38:59 +0900

    [고병수] SBRT 수정사항 적용

--- SBRT-Web/src/main/java/kr/tracom/tims/handler/GetResponse.java
+++ SBRT-Web/src/main/java/kr/tracom/tims/handler/GetResponse.java
@@ -15,6 +15,7 @@ import kr.tracom.platform.attribute.brt.AtTrafficLightStatusResponse;
 import kr.tracom.platform.net.protocol.TimsMessage;
 import kr.tracom.platform.net.protocol.attribute.AtMessage;
 import kr.tracom.platform.net.protocol.payload.PlGetResponse;
+import kr.tracom.tims.domain.HistoryMapper;
 import kr.tracom.ws.WsClient;
 
 @Component
@@ -27,6 +28,9 @@ public class GetResponse {
     
     @Autowired
 	WsClient webSocketClient;
+    
+    @Autowired
+    HistoryMapper historyMapper;
 
 
     public Map<String, Object> handle(TimsMessage timsMessage, String sessionId){
@@ -49,14 +53,55 @@ public class GetResponse {
             		List<HashMap <String, Object>> phaseInfoMapList = new ArrayList<>();
             		AtTrafficLightStatusResponse lightStatus = (AtTrafficLightStatusResponse)atMessage.getAttrData();
             		
-            		
-            		
-    				int phaseNo = lightStatus.getPhaseNumA();
-    				String crsId = lightStatus.getCrossNodeId();
+    				String crsId = lightStatus.getCrossNodeId(); //교차로id
+    				int contSt = lightStatus.getControllerStatus(); //제어기상태
+    				int conMode = lightStatus.getControlMode(); //신호제어 모드
+    				int phaseNoA = lightStatus.getPhaseNumA(); //현시 A
+    				int phaseNoB = lightStatus.getPhaseNumB(); //현지 B
+    				short pahseTmA = lightStatus.getPhaseTimeA(); //현시 진행시간 A
+    				short pahseTmB = lightStatus.getPhaseTimeB(); //현시 진행시간 B
     				
     				HashMap<String, Object> phaseInfoMap = new HashMap<>();
     				phaseInfoMap.put("CRS_ID", crsId);
-    				phaseInfoMap.put("PHASE_NO", phaseNo);
+    				phaseInfoMap.put("CONT_ST", contSt);
+    				phaseInfoMap.put("CONT_MODE", conMode);
+    				phaseInfoMap.put("PHASE_NO", phaseNoA);
+    				phaseInfoMap.put("PHASE_NO_B", phaseNoB);
+    				phaseInfoMap.put("PHASE_TM_A", pahseTmA);
+    				phaseInfoMap.put("PHASE_TM_B", pahseTmB);
+    				//historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				/*phaseInfoMap.put("DATA_VAL", contSt);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK030");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				phaseInfoMap.put("DATA_VAL", conMode);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK039");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				phaseInfoMap.put("DATA_VAL", phaseNoA);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK040");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				phaseInfoMap.put("DATA_VAL", phaseNoB);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK042");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				phaseInfoMap.put("DATA_VAL", pahseTmA);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK041");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);
+    				
+    				phaseInfoMap.put("DATA_VAL", pahseTmB);
+    				phaseInfoMap.put("PARAM_DIV", "PD003");
+    				phaseInfoMap.put("PARAM_KIND", "PK043");
+    				historyMapper.updateFcltCondParamInfo(phaseInfoMap);*/
+    				
+    				
     				phaseInfoMapList.add(phaseInfoMap);
             		
             		
